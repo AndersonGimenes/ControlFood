99c463d57255ce1a83728706e86ee3fbae02a5db
AndersonGimenes <gcosta19@gmail.com>
2020-09-14 23:55:16 -0300

    Refactor

--- ControlFood/ControlFood.Domain/Constantes/Mensagem.cs
+++ ControlFood/ControlFood.Domain/Constantes/Mensagem.cs
@@ -4,13 +4,13 @@
     {
         public static class Validacao
         {
-            public static string PedidoInexistente = "O pedido numero {0} não confere no sistema, por favor verifique o numero do pedido";
+            public const string PedidoInexistente = "O pedido numero {0} não confere no sistema, por favor verifique o numero do pedido";
 
-            public static string CategoriaDuplicada = "A categoria {0} ja existe no sistema";
+            public const string CategoriaDuplicada = "A categoria {0} ja existe no sistema";
 
-            public static string SubCategoriaDuplicada = "A sub-categoria {0} ja existe no sistema";
+            public const string SubCategoriaDuplicada = "A sub-categoria {0} ja existe no sistema";
 
-            public static string CategoriaNaoVinculadaASubCategoria = "Sub-categoria precisa estar vinculada a uma categoria";
+            public const string CategoriaNaoVinculadaASubCategoria = "Sub-categoria precisa estar vinculada a uma categoria";
         }    
     }
 }

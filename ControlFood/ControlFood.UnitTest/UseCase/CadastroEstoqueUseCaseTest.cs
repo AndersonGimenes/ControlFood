1e7cdd1b5c375105b1cdbe22b811ddd6f810003d
AndersonGimenes <gcosta19@gmail.com>
2020-10-26 21:34:56 -0300

    Implementação teste unitario cadastro estoque

---
+++ ControlFood/ControlFood.UnitTest/UseCase/CadastroEstoqueUseCaseTest.cs
@@ -0,0 +1,59 @@
+﻿using ControlFood.Domain.Entidades;
+using ControlFood.UnitTest.UseCase.Helpers;
+using ControlFood.UseCase.Exceptions;
+using ControlFood.UseCase.Implementation;
+using ControlFood.UseCase.Interface.Repository;
+using ControlFood.UseCase.Interface.UseCase;
+using Moq;
+using System;
+using Xunit;
+
+namespace ControlFood.UnitTest.UseCase
+{
+    public class CadastroEstoqueUseCaseTest
+    {
+        private readonly Mock<IEstoqueRepository> _mockEstoqueRepository;
+        private readonly Mock<IProdutoRepository> _mockProdutoRepository;
+        private readonly ICadastroEstoqueUseCase _cadastroEstoqueUseCase;
+
+        public CadastroEstoqueUseCaseTest()
+        {
+            _mockEstoqueRepository = new Mock<IEstoqueRepository>();
+            _mockProdutoRepository = new Mock<IProdutoRepository>();
+            
+            _cadastroEstoqueUseCase = new CadastroEstoqueUseCase(_mockEstoqueRepository.Object, _mockProdutoRepository.Object);
+
+            _mockProdutoRepository
+                .Setup(x => x.BuscarTodos())
+                .Returns(HelperMock.MockListaProdutosPersistidos());
+        }
+
+        [Fact]
+        public void DeveInserirOEstoqueDadoProdutoExistente()
+        {
+            var dataInicio = DateTime.Now;
+
+            var estoqueCocaCola = new Produto();
+            estoqueCocaCola.Estoque = new Estoque { Quantidade = 10, DataValidade = new System.DateTime(2021, 06, 10), ValorCompraTotal = 50.90M, ValorCompraUnidade = 5.09M };
+            estoqueCocaCola.Estoque.AtribuirIdentificadorUnicoProduto(1);
+
+            _cadastroEstoqueUseCase.InserirEstoque(estoqueCocaCola);
+
+            var dataFim = DateTime.Now;
+
+            Assert.True(estoqueCocaCola.Estoque.DataEntrada > dataInicio && estoqueCocaCola.Estoque.DataEntrada < dataFim);
+        }
+
+        [Fact]
+        public void SeNaoHouverProdutoCadastradoDeveLancarUmaException()
+        {
+            var estoqueCocaCola = new Produto();
+            estoqueCocaCola.Estoque = new Estoque { Quantidade = 10, DataValidade = new System.DateTime(2021, 06, 10), ValorCompraTotal = 50.90M, ValorCompraUnidade = 5.09M };
+            estoqueCocaCola.Estoque.AtribuirIdentificadorUnicoProduto(99);
+
+            var ex = Assert.Throws<ProdutoIncorretoUseCaseException>(() => _cadastroEstoqueUseCase.InserirEstoque(estoqueCocaCola));
+            
+            Assert.Equal("Não é possivel cadastrar o estoque: Produto inexistente", ex.Message);
+        }
+    }
+}
--- ControlFood/ControlFood.UseCase/Implementation/CadastroEstoqueUseCase.cs
+++ ControlFood/ControlFood.UseCase/Implementation/CadastroEstoqueUseCase.cs
@@ -1,20 +1,28 @@
-﻿using ControlFood.Domain.Entidades;
+﻿using ControlFood.Domain.Constantes;
+using ControlFood.Domain.Entidades;
+using ControlFood.UseCase.Exceptions;
 using ControlFood.UseCase.Implementation.Base;
 using ControlFood.UseCase.Interface.Repository;
 using ControlFood.UseCase.Interface.UseCase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ControlFood.UseCase.Implementation
 {
     public class CadastroEstoqueUseCase : CadastroBaseUseCase<Estoque>, ICadastroEstoqueUseCase
     {
-        public CadastroEstoqueUseCase(IEstoqueRepository estoqueRepository) 
+        private readonly IProdutoRepository _produtoRepository;
+
+        public CadastroEstoqueUseCase(IEstoqueRepository estoqueRepository, IProdutoRepository produtoRepository) 
             : base(estoqueRepository)
         {
+            _produtoRepository = produtoRepository;
         }
 
         public Produto InserirEstoque(Produto produto)
         {
-            // validar produto x estoque por id 
+            VerificarProdutoVinculado(_produtoRepository.BuscarTodos(), produto);
 
             produto.Estoque.AtribuirDataDeEntrada();
 
@@ -22,6 +30,12 @@ namespace ControlFood.UseCase.Implementation
 
             // ajustar 
             return default;
+        }
+
+        private void VerificarProdutoVinculado(List<Produto> produtos, Produto produto)
+        {
+            if(!produtos.Any(x => x.IdentificadorUnico == produto.Estoque.IdentificadorUnicoProduto))
+                throw new ProdutoIncorretoUseCaseException(Mensagem.Validacao.Produto.ProdutoInexistente);
         }
     }
 }
